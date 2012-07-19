3c0c3e1b40f1e74daaba1a75d47757ea1133f59a
Johns <johnswen91@gmail.com>
2012-07-19 03:04:16 -0500

    Test mandatorios UsuarioAdminTest

---
+++ CRMBenedictoXVI/test/crmbenedictoxvi/CRMBenedictoXVITest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crmbenedictoxvi;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author Johns
+ */
+public class CRMBenedictoXVITest {
+    
+    public CRMBenedictoXVITest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
