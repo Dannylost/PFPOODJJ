9e81735dc536852ffac28be5796b2923c72900a3
Daniel <edanielrr@gmail.com>
2012-07-25 23:24:02 -0500

    Subida 2

--- CRMBenedictoXVI/test/admins/AdminRolTest.java
+++ CRMBenedictoXVI/test/admins/AdminRolTest.java
@@ -1,7 +1,8 @@
 package admins;
 import crmbenedictoxvi.Modulo;
-import crmbenedictoxvi.Permisos;
+import crmbenedictoxvi.Permiso;
 import crmbenedictoxvi.Rol;
+import java.util.ArrayList;
 import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.Assert.*;
@@ -12,7 +13,7 @@ public class AdminRolTest {
     public void validarElNoIngresoDeLosCamposDelFormularioEnVacio(){
         String name = "";
         String desc = "";
-        Permisos permisos = null;
+        Permiso permisos = null;
         
         Assert.assertEquals("", name);
         Assert.assertEquals("", desc);
@@ -25,8 +26,8 @@ public class AdminRolTest {
         String desc = "Usuario normal del sistema del Dpto. de ventas";
         Modulo mod1 = new Modulo(1001,"Clientes");
         Modulo mod2 = new Modulo(1002,"Oportunidad");
-        Permisos permisos1 = new Permisos(mod1,true,true,true,false);
-        Permisos permisos2 = new Permisos(mod2, true, true, true, false);
+        Permiso permisos1 = new Permiso(mod1,true,true,true,false);
+        Permiso permisos2 = new Permiso(mod2, true, true, true, false);
         
         System.out.println("Rol: "+name);
         System.out.println("Desripcion: "+desc);
@@ -36,4 +37,40 @@ public class AdminRolTest {
         System.out.println(permisos2.getModulo().getName()+" --> "+"Acceso="+permisos2.isAcceso() + " Adicionar="+permisos2.isAdicionar()
                 +" Editar="+permisos2.isEditar()+" Eliminar="+permisos2.isEliminar());
     }
+    
+    @Test
+    public void buscarRolEnBaseAParametroDeNombre(){
+        
+        String name="Administrador";
+        AdminRol rol = new AdminRol();
+        Assert.assertNotNull(rol.buscarRol(name));
+    }
+    
+    @Test
+    public void adicionarRolEnElArrayDeBaseDeDatos(){
+        AdminModulo amod = new AdminModulo();
+        ArrayList<Modulo> mod = amod.tablaModulos(); 
+        Modulo clientes = mod.get(0);
+        Permiso permiso = new Permiso(clientes, true, false, true, false); //Modulo de Clientes
+        String name="Usuario";
+        String descrip="Usuario comun y restringido del sistema";
+        
+        AdminRol rol = new AdminRol();
+        Assert.assertNotNull(rol.agregarRol(name,descrip,permiso));
+        
+    }
+    
+    @Test
+    public void eliminarRolEnBaseDeDatosPorNombre(){
+        String name="Administrador";
+        AdminRol rol = new AdminRol();
+        Assert.assertNotNull(rol.eliminarRol(name));
+        System.out.println(rol.eliminarRol(name));
+    }
+    
+    @Test
+    public void editarRolMostrandoPantallaDeEdicionParaActualizarInformacionDelRol(){
+        
+    }
+    
 }
